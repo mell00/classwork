4424928560f418e71873a74a8b157f746c35fcc9
Madison Ell <mellmell2550@gmail.com>
2022-06-15 12:02:13 -0400

    updated to reflect new functions in cplusrtest.cpp

--- REU2022/cplusrtest.h
+++ REU2022/cplusrtest.h
@@ -9,26 +9,31 @@ using namespace Rcpp;
 class Metropolis
 {
 public:
+  void Metropolis();
   double rnorm(int n, double mean, double sd)
   {
     return exp(((n-mean)^2/2*sd^2))/(sd*sqrt(2*pi));
   }
 
-  int repeat(int timesRepeat,int &postMatrixmatrix)
+  double dnorm(int* valArray, double mu, double delta);
 
-  int metroAlgorthm(int timesRepeat2)
+  int repeat(int timesRepeat);
+
+  double logPostDensity(double* mu)
+  int metroAlgorthm(int timesRepeat2);
+
+  const double PI = 3.1415926535;
 
 private:
-  int n;
-  double mean;
-  double sd;
-  double mu;
-  double delta;
-  int df;
-  int ncp;
-  int timesRepeat;
-  int &postMatrix;
-  int &newPostMatrix;
-  double propmu;
-  double mustar;
+  int m_n;
+  double m_mean;
+  double m_sd;
+  int* m_valArray;
+  double* m_mu;
+  double m_delta;
+  int m_df;
+  int m_ncp;
+  int m_timesRepeat;
+  double m_propmu;
+  double m_mustar;
 }
